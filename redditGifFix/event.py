018278de5061331ec8dc4a65b1f0ab56cfb5737f
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-04-07 00:50:39 -0400

    Cleanup event.py and main.py
    Remove old reactcog commands

--- redditGifFix/event.py
+++ redditGifFix/event.py
@@ -21,34 +21,10 @@ class EventMixin(MixinMeta):
         config: dict = await self.config.all_channels()
         if message.channel.id not in config:
             return
-
-        if not config[message.channel.id]["enabled"]:
-            return
-
-        guild_conf: dict = await self.config.guild(message.guild).get_raw()
-        extensions: list = guild_conf["extensions"]
-        websites: list = guild_conf["websites"]
-
-        if not extensions and not websites:
-            return
-
+		
         msg: str = message.content.lower()
-        if all(
-            (not any((ext for ext in extensions if msg.endswith(ext))),
-            not any((site for site in websites if site.lower() in msg)),
-            len(message.attachments) == 0)):
-            return
-
-        if datetime.utcnow().timestamp() >= config[message.channel.id]["next_react_time"] and random.randint(1, 100) <= config[message.channel.id]["multiplier"]:
-            emoji: str = random.choice(config[message.channel.id]["emojis"])
-            try:
-                emoji: discord.Emoji = await commands.EmojiConverter().convert(ctx=await self.bot.get_context(message), argument=emoji)
-            except:
-                emoji: str = emoji
-            finally:
-                try:
-                    await message.add_reaction(emoji)
-                except:
-                    print("Didn't add the emoji, couldn't find it.")
-                new_time: datetime = datetime.utcnow() + timedelta(minutes=random.randint(1, config[message.channel.id]["frequency"]))
-                await self.config.channel(message.channel).set_raw("next_react_time", value=new_time.timestamp())
+		
+		if "preview.redd.it" in msg:
+			msg.replace("preview.redd.it", "i.redd.it")
+			print(msg)
+		
