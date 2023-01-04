4a8fe952ac027eb611c09f568e19fa0b13593bfe
Matt Sheehan <matt-sheehan@outlook.com>
2023-01-03 21:54:00 -0500

    typings

--- certifiedDank/main.py
+++ certifiedDank/main.py
@@ -74,7 +74,7 @@ class certifiedDank(EventMixin, commands.Cog, metaclass=CompositeClass):
 
 
     @certifiedDankAdmin.command()
-    async def channelDankHallCount(self, ctx: commands.Context, channel_id, dankhall_id: int, count: int) -> None:
+    async def channelDankHallCount(self, ctx: commands.Context, channel_id, dankhall_id, count: int) -> None:
         """Change the Hall-of-Fame channel where messages are re-posted (server config)."""
         if type(channel_id) == int:
             channel = ctx.guild.get_channel(channel_id)
